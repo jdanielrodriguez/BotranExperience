e83a28f454f71efec5d025f5c2eb212835eccec6
jdanielrodriguez <daniel.rodriguez@vidoomy.com>
2022-10-24 03:11:59 -0600

    :hammer: added correct assests first version

--- src/components/ARComponents/ARObjects.js
+++ src/components/ARComponents/ARObjects.js
@@ -1,58 +1,58 @@
 
 export default function ARObjects() {
-   const test1 = [
-      {
-         src: require("./../../../assets/images/materiales/01-MODULOS-Intro_Completa-FLATTEN-12a.vrx"),
-         materials: [
-            require('./../../../assets/images/materiales/BOTRAN-No12EtiquetaLOW.png'),
-            require('./../../../assets/images/materiales/Col0-INTRO_Uvmap.png'),
-            require('./../../../assets/images/materiales/Col0-Zoom-Mapa_Uvmap.png'),
-            require('./../../../assets/images/materiales/LANDING_0002_MIXCO-MAP.png'),
-            require('./../../../assets/images/materiales/LANDING_0004_REU-MAP.png'),
-            require('./../../../assets/images/materiales/LANDING_0006_XELA-MAP.png'),
-            require('./../../../assets/images/materiales/ZOOM_Fondo.jpg'),
-         ]
-      },
-      {
-         src: require("./../../../assets/images/materiales/01-MODULOS-Intro_Completa-FLATTEN-12c.vrx"),
-         materials: [
-            require('./../../../assets/images/materiales/BOTRAN-No12EtiquetaLOW.png'),
-            require('./../../../assets/images/materiales/Col0-INTRO_Uvmap.png'),
-            require('./../../../assets/images/materiales/Col0-Zoom-Mapa_Uvmap.png'),
-            require('./../../../assets/images/materiales/LANDING_0002_MIXCO-MAP.png'),
-            require('./../../../assets/images/materiales/LANDING_0004_REU-MAP.png'),
-            require('./../../../assets/images/materiales/LANDING_0006_XELA-MAP.png'),
-            require('./../../../assets/images/materiales/ZOOM_Fondo.jpg'),
-         ]
-      },
-      {
-         src: require("./../../../assets/images/materiales/01-MODULOS-Col3_FLATTEN-12a.vrx"),
-         materials: [
-            require('./../../../assets/images/materiales/BOTRAN-No12Etiqueta3.png'),
-            require('./../../../assets/images/materiales/Col3-AroundTheWorld_Uvmap.png'),
-            require('./../../../assets/images/materiales/Col3-Distribucion_Uvmap.png'),
-            require('./../../../assets/images/materiales/Col3-Embotellado_Uvmap.png'),
-         ]
-      },
-      {
-         src: require("./../../../assets/images/materiales/COL3-12-TEST-Nothing-Emb-06.vrx"),
-         materials: [
-            require('./../../../assets/images/materiales/BOTRAN-No12Etiqueta3.png'),
-            require('./../../../assets/images/materiales/Col3-AroundTheWorld_Uvmap.png'),
-            require('./../../../assets/images/materiales/Col3-Distribucion_Uvmap.png'),
-            require('./../../../assets/images/materiales/Col3-Embotellado_Uvmap.png'),
-         ]
-      },
-      {
-         src: require("./../../../assets/images/materiales/object_soccer_ball.vrx"),
-         materials: [
-            require('./../../../assets/images/materiales/object_soccer_ball_diffuse.png'),
-            require('./../../../assets/images/materiales/object_soccer_ball_normal.png'),
-            require('./../../../assets/images/materiales/object_soccer_ball_specular.png'),
-         ],
-         scale: [.9, .9, .9]
-      }
-   ];
+   // const test1 = [
+   //    {
+   //       src: require("./../../../assets/images/materiales/01-MODULOS-Intro_Completa-FLATTEN-12a.vrx"),
+   //       materials: [
+   //          require('./../../../assets/images/materiales/BOTRAN-No12EtiquetaLOW.png'),
+   //          require('./../../../assets/images/materiales/Col0-INTRO_Uvmap.png'),
+   //          require('./../../../assets/images/materiales/Col0-Zoom-Mapa_Uvmap.png'),
+   //          require('./../../../assets/images/materiales/LANDING_0002_MIXCO-MAP.png'),
+   //          require('./../../../assets/images/materiales/LANDING_0004_REU-MAP.png'),
+   //          require('./../../../assets/images/materiales/LANDING_0006_XELA-MAP.png'),
+   //          require('./../../../assets/images/materiales/ZOOM_Fondo.jpg'),
+   //       ]
+   //    },
+   //    {
+   //       src: require("./../../../assets/images/materiales/01-MODULOS-Intro_Completa-FLATTEN-12c.vrx"),
+   //       materials: [
+   //          require('./../../../assets/images/materiales/BOTRAN-No12EtiquetaLOW.png'),
+   //          require('./../../../assets/images/materiales/Col0-INTRO_Uvmap.png'),
+   //          require('./../../../assets/images/materiales/Col0-Zoom-Mapa_Uvmap.png'),
+   //          require('./../../../assets/images/materiales/LANDING_0002_MIXCO-MAP.png'),
+   //          require('./../../../assets/images/materiales/LANDING_0004_REU-MAP.png'),
+   //          require('./../../../assets/images/materiales/LANDING_0006_XELA-MAP.png'),
+   //          require('./../../../assets/images/materiales/ZOOM_Fondo.jpg'),
+   //       ]
+   //    },
+   //    {
+   //       src: require("./../../../assets/images/materiales/01-MODULOS-Col3_FLATTEN-12a.vrx"),
+   //       materials: [
+   //          require('./../../../assets/images/materiales/BOTRAN-No12Etiqueta3.png'),
+   //          require('./../../../assets/images/materiales/Col3-AroundTheWorld_Uvmap.png'),
+   //          require('./../../../assets/images/materiales/Col3-Distribucion_Uvmap.png'),
+   //          require('./../../../assets/images/materiales/Col3-Embotellado_Uvmap.png'),
+   //       ]
+   //    },
+   //    {
+   //       src: require("./../../../assets/images/materiales/COL3-12-TEST-Nothing-Emb-06.vrx"),
+   //       materials: [
+   //          require('./../../../assets/images/materiales/BOTRAN-No12Etiqueta3.png'),
+   //          require('./../../../assets/images/materiales/Col3-AroundTheWorld_Uvmap.png'),
+   //          require('./../../../assets/images/materiales/Col3-Distribucion_Uvmap.png'),
+   //          require('./../../../assets/images/materiales/Col3-Embotellado_Uvmap.png'),
+   //       ]
+   //    },
+   //    {
+   //       src: require("./../../../assets/images/materiales/object_soccer_ball.vrx"),
+   //       materials: [
+   //          require('./../../../assets/images/materiales/object_soccer_ball_diffuse.png'),
+   //          require('./../../../assets/images/materiales/object_soccer_ball_normal.png'),
+   //          require('./../../../assets/images/materiales/object_soccer_ball_specular.png'),
+   //       ],
+   //       scale: [.9, .9, .9]
+   //    }
+   // ];
 
    const introTexture = [
       require('./../../../assets/vrx/01-FBX-INTRO/BOTRAN-No12EtiquetaLOW.png'),
